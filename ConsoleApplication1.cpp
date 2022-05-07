5aa4c8e0c3db51cb646cc1db7f96f530da69e941
Isaac <39878177+lbyuoe@users.noreply.github.com>
2022-05-07 18:52:00 +0800

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int row = 0, col = 0;
+	cout << "请输出行和列：" << endl;
+	cin >> row >> col;
+	for (int i = 0; i <= row; i++) {
+		if (i == 1) {
+			cout << "|";
+			for (int j = 0; j <= col - 1; j++) {
+				cout << " :-: |";
+			}
+			cout << endl;
+		} else {
+			cout << "|";
+			for (int j = 0; j <= col - 1; j++) {
+				cout << "  |";
+			}
+			cout << endl;
+		}
+	}
+	cin >> row;
+	return 0;
+}
